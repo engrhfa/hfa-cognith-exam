92d39305c5db12cf3217f0c0c621d163b83031f4
HFA <hfalvarado@outlook.com>
2024-08-13 00:12:48 +0800

    Cleanup

--- pokemon-app/src/utils/apiServices.js
+++ pokemon-app/src/utils/apiServices.js
@@ -6,7 +6,6 @@ const fetchPokemonList = async (limit = 20, offset = 0, search = "") => {
   try {
     let url = `${BASE_URL}pokemon?limit=${limit}&offset=${offset}`;
 
-    // If there's a search term, adjust the URL to search for a specific Pokémon
     if (search) {
       url = `${BASE_URL}pokemon/${search.toLowerCase()}`;
     }
@@ -14,7 +13,6 @@ const fetchPokemonList = async (limit = 20, offset = 0, search = "") => {
     const response = await axios.get(url);
 
     if (search) {
-      // If searching by name or number, wrap the result in an array to match the expected format
       return { data: { results: [response.data] } };
     }
 
